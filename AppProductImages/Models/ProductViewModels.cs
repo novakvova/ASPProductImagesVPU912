3abdbc861fefe6c3fd78d6f7dc5441c8e338915d
novakvova <novakvova@gmail.com>
2021-09-12 17:37:38 +0300

    Edit image is good

--- AppProductImages/Models/ProductViewModels.cs
+++ AppProductImages/Models/ProductViewModels.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace AppProductImages.Models
 {
     public class ProductItemViewModel
     {
+        public int Id { get; set; }
         public string Name { get; set; }
         public decimal Price { get; set; }
         public List<ProductImageItemVM> Images { get; set; }
@@ -21,5 +23,20 @@ namespace AppProductImages.Models
         public string Name { get; set; }
         public decimal Price { get; set; }
         public List<IFormFile> Images { get; set; }
+    }
+
+    /// <summary>
+    /// Модель для зміни товару
+    /// </summary>
+    public class ProductEditViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Назва"), Required(ErrorMessage = "Поле 'Назва' не може бути пустим!")]
+        public string Name { get; set; }
+        [Display(Name = "Ціна"), Required(ErrorMessage = "Поле 'Ціна' не може бути пустим!")]
+        public decimal Price { get; set; }
+        [Display(Name = "Фотографії")]
+        public List<IFormFile> Images { get; set; }
+        public List<string> deletedImages { get; set; }
     }
 }
